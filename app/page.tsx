580294ccc869a866ce1e5110c9b892e7123a2eee
rohansinha01 <rohansinha01@gmail.com>
2025-03-11 18:10:43 -0400

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
 
 import { useState } from "react"
 
-
-
+const topNumbers: number[] = [1,2,3,4]
+const bottomNumbers: number[] = [1,2,3,4]
 
 export default function Home() {
-  const [topNumber, setTopNumber] = useState(0)
-  const topNumbers: number[] = []
+  const [topNumber, setTopNumber] = useState(topNumbers)
 
-    return (
-      <>
-     <button onClick={() => setTopNumber(topNumber + 1)}>Add Top Number</button>
-     {topNumbers.push(topNumber)}
-      </>
- )
+  return (
+    <>
+      <div className="grid h-48 place-content-center text-7xl ">
+        {topNumber}  
+      </div>
+      <div className="grid h-48 place-content-center text-7xl">
+        {bottomNumbers}
+      </div>
+      
+      {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+      <button onClick={() => setTopNumber(topNumber.map(num => num + 1))} className="grid place-self-center text-7xl bg-blue-500 hover:bg-blue-700 p-4 rounded-2xl" >
+        Add To Top Row
+      </button>
+      <div>{[topNumbers]}{topNumber}</div>
+    </>
+  );
 }
