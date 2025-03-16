4195fc53b208307148d1bfad3183272da67e3eba
rohansinha01 <rohansinha01@gmail.com>
2025-03-16 17:39:45 -0400

    testing use cases

---
+++ app/typescript-tests/myArray.js
@@ -0,0 +1,8 @@
+var superHeroes = [];
+var heroPower = [];
+var allUsers = [];
+superHeroes.push("spidey");
+heroPower.push(2);
+console.log(superHeroes);
+allUsers.push({ name: "", isActive: true });
+console.log(allUsers);
---
+++ app/typescript-tests/myArray.ts
@@ -0,0 +1,16 @@
+const superHeroes: string[] = []
+const heroPower: Array<number> = []
+
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
+superHeroes.push("spidey")
+heroPower.push(2)
+
+console.log(superHeroes)
+allUsers.push({name: "", isActive: true})
+console.log(allUsers)
