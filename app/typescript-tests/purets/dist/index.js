262f63d9b73176a07c7012fd21e01fdd783c5295
rohansinha01 <rohansinha01@gmail.com>
2025-03-17 16:12:23 -0400

    typescript practice

---
+++ app/typescript-tests/purets/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("typescript is still here");
+console.log("typescript is still amazing");
+console.log("typescript is still amazing");
---
+++ app/typescript-tests/purets/src/index.ts
@@ -0,0 +1,3 @@
+console.log("typescript is still here")
+console.log("typescript is still amazing")
+console.log("typescript is still amazing")
